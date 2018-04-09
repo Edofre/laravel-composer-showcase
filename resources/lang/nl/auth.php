71a46180adb8169ef98dfec3a2494715ff22f26a
Edo Freriks <edofre@gmail.com>
2018-04-09 09:20:30 +0200

    NL translation files

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'                            => 'Deze gegevens komen niet overeen met onze gegevens.',
+    'throttle'                          => 'Te veel login pogingen. Probeer het over :seconds seconden weer.',
+
+    // Login
+    'login'                             => 'Inloggen',
+    'logout'                            => 'Uitloggen',
+    'email'                             => 'E-mail',
+    'email_placeholder'                 => 'E-mail',
+    'password'                          => 'Wachtwoord',
+    'password_placeholder'              => 'Wachtwoord',
+    'forgot_password'                   => 'Wachtwoord vergeten?',
+    'remember_me'                       => "Onthoud mij",
+
+    // Request password reset
+    'reset_password'                    => 'Herstel wachtwoord',
+    'send_password_reset_link'          => 'Verstuur herstel link',
+
+    // Reset password
+    'password_confirmation'             => 'Bevestig wachtwoord',
+    'password_confirmation_placeholder' => 'Bevestig uw wachtwoord',
+
+    // Registration
+    'register'                          => 'Registreer',
+];
---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoord moet tenminste 6 karakters bevatten en gelijk zijn aan de bevestiging.',
+    'reset'    => 'Uw wachtwoord is hersteld!',
+    'sent'     => 'We hebben u een e-mail gestuurd met de link om uw account te herstellen!',
+    'token'    => 'Dit wachtwoord token is ongeldig.',
+    'user'     => "We kunnen geen gebruiker vinden met dit e-mailadres.",
+
+];
